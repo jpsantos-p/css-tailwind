8f502b32081a3f288a60040dc12fa0ee03eaca8b
unknown <joaopinheiro0701@digitalcollege.internal>
2024-11-29 11:59:02 -0300

    brincamos com tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors: {
+            amarelo: '#E6C744',
+            cinza: '#C2C8DA',
+
+          }
+      },
+    },
+    plugins: [],
+  } 
